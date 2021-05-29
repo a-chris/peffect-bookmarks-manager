429ec5da13b454cea04e60eb74481e9ea44a6f4c
achris <a.christian.toscano@gmail.com>
2021-05-30 00:45:03 +0200

    improve readme

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import './Popup.scss';
 
+/* TODO: Not used at the moment */
+
 export default function Popup(): JSX.Element {
   useEffect(() => {
     // Example of how to send a message to eventPage.ts.
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Popup from './Popup';
 
-chrome.tabs.query({ active: true, currentWindow: true }, tab => {
+/* TODO: Not used at the moment */
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
   ReactDOM.render(<Popup />, document.getElementById('popup'));
 });

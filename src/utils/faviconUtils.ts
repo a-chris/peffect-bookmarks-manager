0ca5789d7a0ed8b6571234d3c48f7fa6f2d8ecb2
a-chris <a.christian.toscano@gmail.com>
2021-10-19 23:05:59 +0200

    fetching favicon from remote site

--- src/utils/faviconUtils.ts
+++ src/utils/faviconUtils.ts
@@ -1,5 +1,12 @@
-export default function getFavicon(url?: string | null): string {
-  if (url != null && chrome.bookmarks != null) {
+export default function getFavicon(online: boolean, url?: string | null): string {
+  if (url != null && online) {
+    const { hostname } = new URL(url);
+    return `https://${hostname}/favicon.ico`;
+  } else if (url != null && !online && chrome.bookmarks != null) {
+    /*
+     * WARNING: using chrome://favicon slow down the entire application
+     * I'm avoiding it at the moment
+     */
     return `chrome://favicon/${url}`;
   } else {
     return '';
